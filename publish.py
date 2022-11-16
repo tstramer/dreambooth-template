cf9629c26f03a2f8fe8cc72f4ab526e242097541
Tal Stramer <stramertal@gmail.com>
2022-11-16 13:51:05 -0800

    use docker to build and push

--- publish.py
+++ publish.py
@@ -2,11 +2,17 @@ import os
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import CalledProcessError, check_output
+from typing import Tuple
+import re
+import uuid
 
 import replicate
 
 # This is the location train.py puts the weights it downloads
 DEFAULT_WEIGHTS_PATH = "weights/data/500"
+
+
+MODEL_VERSION_LOG_PATTERN = re.compile("^.*?@sha256:(?P<model_version>.*)$")
 
 
 def publish(model_name: str, weights: Path) -> None:
@@ -42,7 +48,20 @@ def publish(model_name: str, weights: Path) -> None:
 
     # TODO move the weights into the right place
 
-    if not _push_model(model_name):
+    tag_name = str(uuid.uuid4())
+
+    if not _build_model(model_name, tag_name):
+        exit(1)
+
+    if not _push_model(model_name, tag_name):
+        exit(1)
+
+    model_version = _get_model_version(model_name, tag_name)
+    if model_version is None:
+        exit(1)
+    print(f"Model version: {model_version}")
+
+    if not _remove_local_image(model_name, tag_name):
         exit(1)
 
     exit(0)
@@ -50,11 +69,15 @@ def publish(model_name: str, weights: Path) -> None:
 
 # TODO: this doesn't work on 4.14.294-220.533.amzn2.aarch64
 def _cog_installed() -> bool:
-    return _run_command(["command", "-v", "cog"])
+    _, success = _run_command(["command", "-v", "cog"])
+    return success
 
 
 def _cog_login(api_token: str) -> None:
-    return _run_command(["cog", "login", "--token-stdin"], input=api_token.encode())
+    _, success = _run_command(
+        ["cog", "login", "--token-stdin"], input=api_token.encode()
+    )
+    return success
 
 
 def _install_cog() -> None:
@@ -72,16 +95,42 @@ def _model_exists(model_name: str) -> bool:
         return False
 
 
-def _push_model(model_name) -> bool:
-    return _run_command(["cog", "push", f"r8.im/{model_name}"])
+def _build_model(model_name, tag_name) -> bool:
+    _, success = _run_command(["cog", "build", "-t", f"r8.im/{model_name}:{tag_name}"])
+    return success
 
 
-def _run_command(*args, **kwargs) -> bool:
+def _push_model(model_name, tag_name) -> bool:
+    _, success = _run_command(["docker", "push", f"r8.im/{model_name}:{tag_name}"])
+    return success
+
+
+def _remove_local_image(model_name, tag_name) -> bool:
+    _, success = _run_command(
+        ["docker", "image", "rm", f"r8.im/{model_name}:{tag_name}"]
+    )
+    return success
+
+
+def _get_model_version(model_name, tag_name) -> str:
+    command = f"docker inspect --format='{{{{index .RepoDigests 0}}}}' r8.im/{model_name}:{tag_name}"
+    output, success = _run_command(command, shell=True)
+    if not success:
+        return None
+
+    output = output.decode("utf8").strip()
+    match = MODEL_VERSION_LOG_PATTERN.match(output)
+    if match is None:
+        return None
+    return match.group("model_version")
+
+
+def _run_command(*args, **kwargs) -> Tuple[any, bool]:
     try:
-        check_output(*args, **kwargs)
-        return True
+        output = check_output(*args, **kwargs)
+        return output, True
     except CalledProcessError:
-        return False
+        return None, False
 
 
 if __name__ == "__main__":
